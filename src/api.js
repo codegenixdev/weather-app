e98b8a12487ff465bf4734c1df3d890a9f6fe8b4
Amir Hosein Nazari <czhablog@gmail.com>
2022-10-31 18:29:52 +0400

    feat: initial commit

---
+++ src/api.js
@@ -0,0 +1,11 @@
+export const geoApiOptions = {
+  method: "GET",
+  url: "https://wft-geo-db.p.rapidapi.com/v1/geo/adminDivisions",
+  headers: {
+    "X-RapidAPI-Key": "719c570463msh84dc589aa5680bbp1a0867jsn1581b1ec395a",
+    "X-RapidAPI-Host": "wft-geo-db.p.rapidapi.com",
+  },
+};
+
+export const GEO_API_URL =
+  "https://wft-geo-db.p.rapidapi.com/v1/geo/adminDivisions";
---
+++ src/components/search/search.js
@@ -0,0 +1,33 @@
+import { useState } from "react";
+import { AsyncPaginate } from "react-select-async-paginate";
+import { GEO_API_URL, geoApiOptions } from "../../api";
+import axios from "axios";
+
+const Search = ({ onSearchChange }) => {
+  const [search, setSearch] = useState(null);
+
+  const loadOptions = (inputValue) => {
+    axios
+      .request(geoApiOptions)
+      .then(function (response) {
+        console.log(response.data);
+      })
+      .catch(function (error) {
+        console.error(error);
+      });
+  };
+  const handleOnChange = (searchData) => {
+    setSearch(searchData);
+    onSearchChange(searchData);
+  };
+  return (
+    <AsyncPaginate
+      placeholder="Search for city"
+      debounceTimeout={600}
+      value={search}
+      onChange={handleOnChange}
+      loadOptions={loadOptions}
+    />
+  );
+};
+export default Search;
